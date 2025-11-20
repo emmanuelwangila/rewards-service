0140a62f821a91bdf941c7f340af9e4e73305427
Emmanuel Wangila <emmanuelwangila1@gmail.com>
2025-11-20 22:31:29 +0300

    API key auth filter file

---
+++ src/main/java/com/rewards_service/infrastructure/config/ApiKeyAuthFilter.java
@@ -0,0 +1,43 @@
+package com.rewards_service.infrastructure.config;
+
+import com.rewards_service.application.service.UserService;
+import com.rewards_service.domain.model.UserAccount;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String apiKey = request.getHeader("X-API-Key");
+        if (apiKey != null) {
+            Optional<UserAccount> userOpt = userService.findByApiKey(apiKey);
+            if (userOpt.isPresent()) {
+                UserAccount user = userOpt.get();
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(user, null, null);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
