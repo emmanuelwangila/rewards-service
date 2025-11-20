79063eadc0147ab2fa182f7cac751337f9958c02
Emmanuel Wangila <emmanuelwangila1@gmail.com>
2025-11-20 22:27:03 +0300

    Web Config with interceptors

---
+++ src/main/java/com/rewards_service/infrastructure/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.rewards_service.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**", "/actuator/**");
+    }
+}
