99dbb8508c5c263395d64920722f69f85ff0b509
Emmanuel Wangila <emmanuelwangila1@gmail.com>
2025-11-21 13:17:40 +0300

    Fix testCointainers failures

--- src/test/java/com/rewards_service/TestcontainersConfiguration.java
+++ src/test/java/com/rewards_service/TestcontainersConfiguration.java
@@ -3,10 +3,12 @@ package com.rewards_service;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Profile("test")
 class TestcontainersConfiguration {
 
     @Bean
